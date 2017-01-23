5b157193201e6c83404fd1d48c6510cdc3a4b50f
wmzhong <zhongwm.net@gmail.com>
2017-01-23 21:33:12 +0800

    完成 timer 原型

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,25 +2,14 @@
 //获取应用实例
 var app = getApp()
 Page({
-  data: {
-    motto: 'Tomato Timer',
-    userInfo: {}
-  },
   //事件处理函数
-  bindViewTap: function() {
+  bindViewTap: function(event) {
+    console.log(event)
     wx.navigateTo({
       url: '../timer/timer'
     })
   },
   onLoad: function () {
     console.log('onLoad')
-    var that = this
-    //调用应用实例的方法获取全局数据
-    app.getUserInfo(function(userInfo){
-      //更新数据
-      that.setData({
-        userInfo:userInfo
-      })
-    })
   }
 })
