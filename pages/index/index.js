060c1dc40b9330bebea92595ad37a40474c62d7b
wmzhong <zhongwm.net@gmail.com>
2017-01-24 12:19:42 +0800

    完成点击按钮元素消失功能

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,11 +2,15 @@
 //获取应用实例
 var app = getApp()
 Page({
+  data: {
+    display: ''
+  },
+
   //事件处理函数
   bindViewTap: function(event) {
     console.log(event)
-    wx.navigateTo({
-      url: '../timer/timer'
+    this.setData({
+      display: 'display: none;'
     })
   },
   onLoad: function () {
