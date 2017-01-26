a8e42b3105b29c1bdf1eb774c2db460cd165c24d
wmzhong <zhongwm.net@gmail.com>
2017-01-26 11:30:40 +0800

    完成扇形图

--- pages/count/count.js
+++ pages/count/count.js
@@ -1,11 +1,56 @@
 var util = require('../../utils/util.js')
+var wxCharts = require('../../utils/wxcharts.js')
+var res = wx.getSystemInfoSync();
+var windowWidth = res.windowWidth;
+
+var pieCharts = new wxCharts({
+  animation: true,
+  canvasId: 'pieCanvas',
+  type: 'pie',
+  series: [{
+      name: '成交量1',
+      data: 15,
+    }, {
+      name: '成交量2',
+      data: 35,
+    }, {
+      name: '成交量3',
+      data: 78,
+    }, {
+      name: '成交量4',
+      data: 63,
+    }, {
+      name: '成交量2',
+      data: 35,
+    }, {
+      name: '成交量3',
+      data: 78,
+    }, {
+      name: '成交量4',
+      data: 63,
+    }, {
+      name: '成交量2',
+      data: 35,
+    }, {
+      name: '成交量3',
+      data: 78,
+    }, {
+      name: '成交量3',
+      data: 78,
+  }],
+  width: windowWidth,
+  height: 300,
+  dataLabel: false,
+})
+
 
 Page({
   data:{
     date: util.formatTime(new Date())
   },
+
   onLoad:function(options){
-    // 生命周期函数--监听页面加载
-    String2
+    console.log('count on load')
+    pieCharts
   }
 })
